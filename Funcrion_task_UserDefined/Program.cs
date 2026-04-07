8b8374d4736e48b0c168c0dbaed54fafc078d53a
HebatAllah.ss <HebatAllah.ss@MEMLT0047>
2026-04-07 12:39:10 +0400

    update

---
+++ Funcrion_task_UserDefined/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Funcrion_task_UserDefined
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
