a9e4fb08351e970665fb7d3fd3e7d41457bd67c5
Hebatallah <hibatullah.saleh2000@gmail.com>
2026-04-07 12:10:22 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Function_task_User_defined__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a user-defined function example in C# 14.0.");
+
+
+        }
+    }
+}
