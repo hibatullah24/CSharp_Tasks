fee73886a12af1529c872569cb4574be5c80be4c
HebatAllah.ss <HebatAllah.ss@MEMLT0047>
2026-04-07 11:54:43 +0400

    orgnize file

---
+++ Function Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Function_task_User_defined__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
